feee7a8bb79d1215be7a55bfbe8e9204b078bf61
Crypto-Spartan <accounts@cryptospartan.net>
2021-12-12 07:21:49 +0000

    move round_trip_compression_decompression() to mod test

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub fn test_linkage() {
     println!("Link to lib.rs working!");
 }
 
-/*
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -20,35 +20,58 @@ mod test {
     #[test]
     fn round_trip_compression_decompression() {
         unsafe {
-            let text = "This is a simple example on how to use the simple Density API.".as_bytes();
-            let text_length = text.len() as uint_fast64_t;
-            let text_ptr = text.as_ptr();
+            let text = "This is a simple example on how to use the simple Density API. (Here's some data to make this string longer) qwertyuiop[]asdfghjkl;:zxcvbnm,<.>/?`~1!2@3#4$5%6^7&8*9(0)-_=+".to_owned();
 
-            let compress_safe_size = density_compress_safe_size(text_length);
-            let decompress_safe_size = density_decompress_safe_size(text_length);
+            let text_bytes = text.as_bytes();
+            println!("text_bytes = {}", std::str::from_utf8(text_bytes.clone()).unwrap());
+            let text_length = text_bytes.len();
+            println!("text_length = {}", text_length);
 
-            let mut compressed_output: Vec<u8> = vec![0; text.len()];
-            let mut decompressed_output: Vec<u8> = vec![0; text.len()];
-            let mut compressed_output_ptr = compressed_output.as_mut_ptr();
-            let mut decompressed_output_ptr = decompressed_output.as_mut_ptr();
+            let compress_safe_size = density_compress_safe_size(text_length as _);
+            let decompress_safe_size = density_decompress_safe_size(text_length as _);
+            println!(
+                "compress_safe_size = {}\ndecompress_safe_size = {}",
+                compress_safe_size, decompress_safe_size
+            );
 
-            let result: density_processing_result = density_compress(text_ptr, text_length, compressed_output_ptr, compress_safe_size, DENSITY_ALGORITHM_DENSITY_ALGORITHM_CHAMELEON);
+            let mut compressed_output: Vec<u8> = vec![0; compress_safe_size as usize];
+            let mut decompressed_output: Vec<u8> = vec![0; decompress_safe_size as usize];
 
-            /*if result.state == DENSITY_STATE_DENSITY_STATE_OK {
+            let result: density_processing_result = density_compress(
+                text_bytes.as_ptr() as _, 
+                text_length as _, 
+                compressed_output.as_mut_ptr() as *mut _, 
+                compress_safe_size as _, 
+                DENSITY_ALGORITHM_DENSITY_ALGORITHM_CHAMELEON
+            );
+
+            if result.state == DENSITY_STATE_DENSITY_STATE_OK {
                 println!("Compressed {} bytes to {} bytes", result.bytesRead, result.bytesWritten);
             } else {
-                panic!("Compression Error");
-            }*/
+                //result.state is a u32
+                let error = read_density_error(result.state);
+                panic!("Compression Error: {}", error);
+            }
 
-            //let result: density_processing_result = density_decompress(compressed_output_ptr, result.bytesWritten, decompressed_output_ptr, decompress_safe_size);
+            let result: density_processing_result = density_decompress(
+                compressed_output.as_mut_ptr() as *mut _, 
+                result.bytesWritten as _, 
+                decompressed_output.as_mut_ptr() as *mut _,
+                decompress_safe_size as _
+            );
 
-            /*if result.state == DENSITY_STATE_DENSITY_STATE_OK {
+            if result.state == DENSITY_STATE_DENSITY_STATE_OK {
                 println!("Deompressed {} bytes to {} bytes", result.bytesRead, result.bytesWritten);
+                decompressed_output.truncate(result.bytesWritten as _);
             } else {
-                panic!("Deompression Error");
-            }*/
+                let error = read_density_error(result.state);
+                panic!("Deompression Error: {}", error);
+            }
 
-            //assert!(text == decompressed_output);
+            println!("text_bytes = {:?}\ndecompressed_output = {:?}", text_bytes, decompressed_output);
+            assert!(text_bytes == &decompressed_output);
+
+            // c function signatures for reference
 
             /*pub fn density_compress(
                 input_buffer: *const u8,
@@ -57,8 +80,25 @@ mod test {
                 output_size: uint_fast64_t,
                 algorithm: DENSITY_ALGORITHM,
             ) -> density_processing_result*/
-            
+
+            /*pub fn density_decompress(
+                input_buffer: *const u8,
+                input_size: uint_fast64_t,
+                output_buffer: *mut u8,
+                output_size: uint_fast64_t,
+            ) -> density_processing_result;*/
+        }   
+    }
+
+    fn read_density_error(err_code: u32) -> String {
+        match err_code {
+            1u32 => "DENSITY_STATE_ERROR_INPUT_BUFFER_TOO_SMALL".to_owned(),
+            2u32 => "DENSITY_STATE_ERROR_OUTPUT_BUFFER_TOO_SMALL".to_owned(),
+            3u32 => "DENSITY_STATE_ERROR_DURING_PROCESSING".to_owned(),
+            4u32 => "DENSITY_STATE_ERROR_INVALID_CONTEXT".to_owned(),
+            5u32 => "DENSITY_STATE_ERROR_INVALID_ALGORITHM".to_owned(),
+            _ => unreachable!()
         }
     }
         
-}*/
+}
