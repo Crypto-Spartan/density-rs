06c41644ddd0d712d72bd4b671d7bafd698db54a
Crypto-Spartan <accounts@cryptospartan.net>
2021-12-12 07:17:56 +0000

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,14 @@ use std::mem;
 
 
 fn main() {
-    //test_linkage();
-    //println!("UINT_LEAST32_MAX: {}", UINT_LEAST32_MAX);
-    //let text = "This is a simple example on how to use the simple Density API.".as_bytes();
-    //println!("{:?}", text);
     unsafe {
         round_trip_compression_decompression();
-        /*let mut prev = 0;
-        let mut diff;
-        for i in (0..1_048_576).step_by(8192*2) {
-            let buf = density_compress_safe_size(i as _);
-            diff = buf - prev;
-            println!("i={} buf={} diff={}", i, buf, diff);
-            prev = buf;
-        }*/
     }
 }
 
 
 unsafe fn round_trip_compression_decompression() {
-    let text = "This is a simple example on how to use the".to_owned();// simple Density API....d.....a.........b.....z....e...qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmThis is a simple example on how to use the simple Density API....d.....a.........b.....z....e...qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmThis is a simple example on how to use the simple Density API....d.....a.........b.....z....e...qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmThis is a simple example on how to use the simple Density API....d.....a.........b.....z....e...qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmThis is a simple example on how to use the simple Density API....d.....a.........b.....z....e...qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm".as_bytes();
+    let text = "This is a simple example on how to use the simple Density API. (Here's some data to make this string longer) qwertyuiop[]asdfghjkl;:zxcvbnm,<.>/?`~1!2@3#4$5%6^7&8*9(0)-_=+".to_owned();
 
     let text_bytes = text.as_bytes();
     println!("text_bytes = {}", std::str::from_utf8(text_bytes.clone()).unwrap());
@@ -39,8 +27,6 @@ unsafe fn round_trip_compression_decompression() {
 
     let mut compressed_output: Vec<u8> = vec![0; compress_safe_size as usize];
     let mut decompressed_output: Vec<u8> = vec![0; decompress_safe_size as usize];
-    //let mut compressed_output_ptr = (&compressed_output).as_mut_ptr();
-    //let mut decompressed_output_ptr = (&decompressed_output).as_mut_ptr();
 
     let result: density_processing_result = density_compress(
         text_bytes.as_ptr() as _, 
@@ -50,17 +36,13 @@ unsafe fn round_trip_compression_decompression() {
         DENSITY_ALGORITHM_DENSITY_ALGORITHM_CHAMELEON
     );
 
-    println!("Rust FLAG #1");
-
     if result.state == DENSITY_STATE_DENSITY_STATE_OK {
         println!("Compressed {} bytes to {} bytes", result.bytesRead, result.bytesWritten);
     } else {
+        //result.state is a u32
         let error = read_density_error(result.state);
-        //result.state.f(); - u32
         panic!("Compression Error: {}", error);
     }
-
-    println!("Rust FLAG #2");
 
     let result: density_processing_result = density_decompress(
         compressed_output.as_mut_ptr() as *mut _, 
@@ -68,8 +50,6 @@ unsafe fn round_trip_compression_decompression() {
         decompressed_output.as_mut_ptr() as *mut _,
         decompress_safe_size as _
     );
-
-    println!("Rust FLAG #3");
 
     if result.state == DENSITY_STATE_DENSITY_STATE_OK {
         println!("Deompressed {} bytes to {} bytes", result.bytesRead, result.bytesWritten);
@@ -79,10 +59,10 @@ unsafe fn round_trip_compression_decompression() {
         panic!("Deompression Error: {}", error);
     }
 
-    println!("Rust FLAG #4");
     println!("text_bytes = {:?}\ndecompressed_output = {:?}", text_bytes, decompressed_output);
-
     assert!(text_bytes == &decompressed_output);
+
+    // c function signatures for reference
 
     /*pub fn density_compress(
         input_buffer: *const u8,
