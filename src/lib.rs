d9c98dfeb8b62c5476be38e779f87b9b73d28991
Crypto-Spartan <accounts@cryptospartan.net>
2021-12-13 17:55:38 +0000

    add decompress() to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@
 #[macro_use]
 pub mod utils;
 
-pub mod c_bindings;
+#[cfg(test)]
 pub mod tests;
 
+pub mod c_bindings;
 
-//density_processing_result density_compress(const uint8_t *input_buffer, const uint_fast64_t input_size, uint8_t *output_buffer, const uint_fast64_t output_size, const DENSITY_ALGORITHM algorithm) 
 
 pub fn compress(
     input_buffer: &[u8], 
@@ -39,7 +39,7 @@ pub fn compress(
     return result;
 }
 
-/*density_processing_result density_decompress(const uint8_t *input_buffer, const uint_fast64_t input_size, uint8_t *output_buffer, const uint_fast64_t output_size) 
+
 
 pub fn decompress(
     input_buffer: &[u8], 
@@ -47,27 +47,28 @@ pub fn decompress(
     output_buffer: &mut [u8], 
     output_size: u64
 ) -> c_bindings::density_processing_result {
+
+    let input_buffer_ptr = input_buffer.as_ptr() as _;
+    let output_buffer_ptr = output_buffer.as_mut_ptr() as *mut _;
     
-    density_processing_result result = density_decompress_prepare_context(input_buffer, input_size, false, malloc);
-    if(result.state) {
-        density_free_context(result.context);
+    let mut result: c_bindings::density_processing_result = unsafe {
+        c_bindings::density_decompress_prepare_context(input_buffer_ptr, input_size, false)
+    };
+    if result.state > 0u32 {
+        unsafe { c_bindings::density_free_context(result.context); }
         return result;
     }
 
-    result = density_decompress_with_context(input_buffer + result.bytesRead, input_size - result.bytesRead, output_buffer, output_size, result.context);
-    density_free_context(result.context);
-    return result;
-*/
-/*
-
-DENSITY_WINDOWS_EXPORT density_processing_result density_decompress(const uint8_t *input_buffer, const uint_fast64_t input_size, uint8_t *output_buffer, const uint_fast64_t output_size) {
-    density_processing_result result = density_decompress_prepare_context(input_buffer, input_size, false, malloc);
-    if(result.state) {
-        density_free_context(result.context);
-        return result;
+    unsafe {
+        result = c_bindings::density_decompress_with_context(
+            input_buffer_ptr.offset(result.bytesRead.try_into().unwrap()), 
+            input_size - result.bytesRead, 
+            output_buffer_ptr, 
+            output_size, 
+            result.context
+        );
+        c_bindings::density_free_context(result.context);
     }
-
-    result = density_decompress_with_context(input_buffer + result.bytesRead, input_size - result.bytesRead, output_buffer, output_size, result.context);
-    density_free_context(result.context);
+    
     return result;
-}*/
+}
